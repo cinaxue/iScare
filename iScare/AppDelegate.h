8e860546e944955c20f277bd5ecfe318fa72b533
Cina <Cina@localhost.(none)>
2012-08-06 15:22:42 +0800

    Initial Commit

---
+++ iScare/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iScare
+//
+//  Created by Cina on 8/6/12.
+//  Copyright (c) 2012 Cina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iScare/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iScare
+//
+//  Created by Cina on 8/6/12.
+//  Copyright (c) 2012 Cina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
